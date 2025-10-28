66551f079b9601b99245873355c5b2c14baac3b3
Gotoda <wa900622@gmail.com>
2025-10-28 16:29:02 +0900

    Fix FizzBuzz logic

---
+++ 4.コードの修正対応実践/FizzBuzz/code.cs
@@ -0,0 +1,18 @@
+// C#のFizzBuzz
+using System;
+
+public class FizzBuzz {
+    public static void Main(string[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                Console.WriteLine("Fizz");
+            } else if (i % 5 == 0) {
+                Console.WriteLine("Buzz");
+            } else if (i % 15 == 0){
+                Console.WriteLine("FizzBuzz");
+            } else {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
