5de3b34d24531e1ca4b161a760ab9051fda99335
Saka-Masa-321 <smamasaru@gmail.com>
2025-10-28 16:34:43 +0900

    code.goの()を削除と3又は5で割り切れる場合の判定コードを修正

--- 4. コードの修正対応実践/FizzBuzz/code.go
+++ 4. コードの修正対応実践/FizzBuzz/code.go
@@ -4,8 +4,13 @@ package main
 import "fmt"
 
 func main() {
+<<<<<<< HEAD
 	for i := 1; i <= 100; i++ {
 		if i%3 == 0 && i%5 == 0 {
+=======
+	for (i := 1; i <= 100; i++) {
+		if i%15 == 0 {
+>>>>>>> parent of 597b686 (code.go)
 			fmt.Println("FizzBuzz")
 		} else if i%3 == 0 {
 			fmt.Println("Fizz")
