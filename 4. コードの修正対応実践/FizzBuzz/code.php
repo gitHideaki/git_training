5c19659cc38cffa0b81e191b79b8c9e9e7239b3d
Suga568 <sugamiya6@gmail.com>
2025-10-28 16:41:46 +0900

    Update code.php
    修正

--- 4. コードの修正対応実践/FizzBuzz/code.php
+++ 4. コードの修正対応実践/FizzBuzz/code.php
@@ -1,8 +1,10 @@
 <?php
-// PHPのFizzBuzz
+// PHPのFizzBuzz（修正版）
 
 for ($i = 1; $i <= 100; $i++) {
-  if ($i % 3 == 0) {
+  if ($i % 15 == 0) {
+    echo "FizzBuzz\n";
+  } elseif ($i % 3 == 0) {
     echo "Fizz\n";
   } elseif ($i % 5 == 0) {
     echo "Buzz\n";
